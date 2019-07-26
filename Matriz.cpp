23fa4fc8f661b3885fc4da7a0a192720f7551900
miguelblanco784 <miguelrobertoblanco@gmail.com>
2019-07-26 15:56:27 -0600

    Push2

--- Matriz.cpp
+++ Matriz.cpp
@@ -18,29 +18,105 @@ class Matriz {
 	protected:
 	
 	public:
-		void llenarrandom();
+		void llenarrandom(int an, int al);
 		
-		matriz(){
-			llenadorandom();
+		Matriz(){
+			llenadorandom(5, 5);
 			ancho = 5;
 			altura = 5;
+		}
+		
+		Matriz(int a, int b){
+			ancho = a;
+			altura = b;
+			llenadorandom(a, b);
+		}
+		
+		bool Suma(Matriz p){
+			bool k = false;
+			if(p.getAncho() == ancho && p.getAltura() == altura){
+				for(int ab = 0; ab < ancho; ab++){
+					for(int ba = 0; ba < altura ;ba++){
+						matrizz[ab][ba] = matrizz[ab][ba]+p.getMatrizz()[ab][ba];
+						cout<<"["<<matrizz[ab][ba]<<"]";
+						
+					}
+					cout<<endl;
+				}
+				k = true;
+			}else{
+				k = false;
+			}
+			return k;
 		}	
 		
-		void llenadorandom(){
+		bool Resta(Matriz p){
+			bool k = false;
+			if(p.getAncho() == ancho && p.getAltura() == altura){
+				for(int ab = 0; ab < ancho; ab++){
+					for(int ba = 0; ba < altura ;ba++){
+						matrizz[ab][ba] = matrizz[ab][ba]-p.getMatrizz()[ab][ba];
+						cout<<"["<<matrizz[ab][ba]<<"]";
+						
+					}
+					cout<<endl;
+				}
+				k = true;
+			}else{
+				k = false;
+			}
+			return k;
+		}
+		
+		bool Multiplicacion(int jk){
+			bool k = false;
+				for(int ab = 0; ab < ancho; ab++){
+					for(int ba = 0; ba < altura ;ba++){
+						matrizz[ab][ba] = matrizz[ab][ba]*jk;
+						cout<<"["<<matrizz[ab][ba]<<"]";
+						
+					}
+					cout<<endl;
+				}
+				k = true;
+			return k;
+		}
+		
+		void llenadorandom(int an, int al){
 			srand(time(NULL));
-			matrizz = new int *[5];
-			matrizz[0] = new int [5];
-			matrizz[1] = new int [5];
-			matrizz[2] = new int [5];
-			matrizz[3] = new int [5];
-			matrizz[4] = new int [5];
-			for(int a = 0;a < 5;a++){
-				for(int b = 0;b < 5;b++){
+			matrizz = new int *[al];
+			for(int a = 0; a < al; a++){
+				matrizz[a] = new int [an]; 
+			}
+			
+			for(int a = 0;a < al;a++){
+				for(int b = 0;b < an;b++){
 					int temp;
 					temp = rand()%10+1;
 					matrizz[a][b] = temp;
 				}
 			}			
-		}	
+		}
+		
+		int getAltura(){
+			return altura;
+		}
+		
+		int setAltura(int x){
+			altura = x;
+		}
+		
+		int getAncho(){
+			return ancho;
+		}
+		
+		int setAncho(int x){
+			ancho = x;
+		}
+		
+		int** getMatrizz(){
+			return matrizz;
+		}
+			
 };
 #endif
--- Principal.cpp
+++ Principal.cpp
@@ -5,17 +5,64 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 #include <time.h>
+#include "Matriz.cpp"
 
 using namespace std;
 
 int main(){
-	srand(time(NULL));
-	int num;
-	num = rand()%10+1;
-	cout<<num;
+	cout<<"Ingrese que tipo de matriz quiere\n1- 5x5 Aleatoria\n2- Definir tamano aleatoria\n";
+	int opcion1;
+	Matriz *p;
+	cin>>opcion1;
+	if(opcion1 == 1){
+		Matriz *p = new Matriz();
+	}else{
+		int a, b;
+		cout<<"Ingrese 1:";
+		cin>>a;
+		cout<<"Ingrese 2:";
+		cin>>b;
+		Matriz *p = new Matriz(a,b);
+	}
 	
+		int opcion2;
+		cout<<"1- Multiplicar\n2- Sumar\n3- Restar";
+		cin>>opcion2;
+		switch(opcion2){
+			case 1:
+				int temp1;
+				cout<<"Ingrese su multiplicando";
+				cin>>temp1;
+				cout<<p -> Multiplicacion(temp1)<<endl;
+				break;
+			case 2:
+				int j, k;
+				cout<<"Ingrese 1: ";
+				cin>>j;
+				cout<<"Ingrese 2: ";
+				cin>>k;
+				Matriz *l = new Matriz(j,k);
+				cout<<p -> Suma(*l)<<endl;
+				l -> ~Matriz();
+				break;
+			case 3:
+				int ja, ka;
+				cout<<"Ingrese 1: ";
+				cin>>ja;
+				cout<<"Ingrese 2: ";
+				cin>>ka;
+				Matriz *ja = new Matrizz(j,k);
+				cout<<p -> Suma(*ja)<<endl;
+				ja -> ~Matriz();
+				break;
+		}	
+	
+	/*Matriz *p = new Matriz(4,4);
+	Matriz *j = new Matriz(4,4);
+	cout<<p -> Suma(*j)<<endl;
+	cout<<p -> Resta(*j)<<endl;
+	cout<<p -> Multiplicacion(*j)<<endl;*/
 	return 0;
-	system("pause");
 }
 
 
